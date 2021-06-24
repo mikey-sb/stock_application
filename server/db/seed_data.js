d753586dff7edcca08bd4bc23a539f43be6d7c65
saadtarikk <82512601+saadtarikk@users.noreply.github.com>
2021-06-24 21:21:58 +0100

    server.js Router and Services added

--- server/db/seed_data.js
+++ server/db/seed_data.js
@@ -1,4 +1,4 @@
-use stocks_db
+use shares;
 db.dropDatabase();
 
 db.stocks.insertMany(
@@ -9,6 +9,12 @@ db.stocks.insertMany(
             total_stocks_value: 0, // stock value x buy_amount
             transaction_history: {} // [{Amazon... buy_price buy_time}]
         },
+        { 
+            total_cash: 50, // - 20 (input)
+            stocks_held: [], // [{Amazon...}]
+            total_stocks_value: 0, // stock value x buy_amount
+            transaction_history: [] // [{Amazon... buy_price buy_time}]
+        }
     ]
 )
 
---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const app = express()
+const MongoClient = require('mongodb').MongoClient
+const createRouter = require('./helpers/create_router.js')
+const cors = require('cors')
+
+app.use(express.json())
+app.use(cors())
+
+MongoClient.connect('mongodb://localhost:27017', { useUnifiedTopology: true })
+.then((client) => {
+    const db = client.db('shares')
+    const stocksCollection = db.collection('stocks')
+    const stocksRouter = createRouter(stocksCollection)
+    app.use('/api/stocks', stocksRouter)
+})
+.catch(console.error)
+
+app.listen(5000, function() {
+    console.log(`Listening on port ${this.address().port}`); })
