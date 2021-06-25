a00c476a0c5009685a1eaaab57b4d1c32d3ecde6
saadtarikk <82512601+saadtarikk@users.noreply.github.com>
2021-06-25 14:48:54 +0100

    setup wallet component

---
+++ client/src/components/profile_components/Wallet.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+
+const Wallet = () => {
+
+    const [total, setTotal]
+
+
+    return (
+        <div>
+            <h1>Wallet</h1>
+        </div>
+    )
+}
+
+export default wallet
