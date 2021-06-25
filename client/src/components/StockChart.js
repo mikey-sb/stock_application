bb4bd9d7e38c91359b8e50185a49b2e6a1a00ead
michael403053 <michael.christie5@hotmail.co.uk>
2021-06-25 14:27:02 +0100

    working main branch DO NOT NOT BRANCH

---
+++ client/src/components/StockChart.js
@@ -0,0 +1,41 @@
+import React, {useState, useEffect} from 'react'
+import HighCharts from 'highcharts'
+import HighchartsReact from 'highcharts-react-official';
+const StockChart = ({allStock}) => {
+   
+    const options = {
+        series: [{
+            name: 'Profit',
+            data: []
+        }]
+    }
+    const setChartOptions = () => {
+        if(allStock){
+            options["series"][0]["name"] = allStock["Meta Data"]["2. Symbol"]
+            for(let banana in allStock["Time Series (5min)"]){
+                let pricePoint = allStock["Time Series (5min)"][banana]["1. open"]
+                options["series"][0]["data"].push(Number(pricePoint))
+            }
+            console.log(options)
+        }
+    }
+    const renderChart = () => {
+        if(allStock){
+            setChartOptions()
+            return <HighchartsReact highcharts={HighCharts} options={options} />
+        }
+    }
+    // console.log(allStock["Time Series (5min)"])
+    return (
+        <>
+            <div className="data-container">
+            <ul>
+                <li>{allStock ? allStock["Meta Data"]["2. Symbol"] : null}</li>
+                <li>{allStock ? allStock["Time Series (5min)"]["2021-06-23 20:00:00"]["1. open"] : null}</li>
+            </ul>
+            {renderChart()}
+            </div>
+        </>
+    )
+}
+export default StockChart
