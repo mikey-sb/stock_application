dede832a36e8d9cdb5554187fb1b10f122629b60
michael403053 <michael.christie5@hotmail.co.uk>
2021-06-26 14:57:16 +0100

    update

--- client/src/components/CandleStickChart.js
+++ client/src/components/CandleStickChart.js
@@ -18,10 +18,15 @@ const CandleStickChart = ({allStock}) => {
         // set the allowed units for data grouping
         groupingUnits = [[
             'min',                         // unit name
-            [5, 10, 15]                             // allowed multiples
+            [5, 10, 15, 30]                             // allowed multiples
+        ],
+        ['hour', 
+            [1, 4]
         ],
         ['day',
-            [1, 2, 3, 4, 6, 7]]
+            [1, 3]
+        ],
+        ['week', [1, 2]]
         ]
 
         
