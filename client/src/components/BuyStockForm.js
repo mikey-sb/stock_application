3cd2bc32336ea4e2117f7fefcbfe1d0170600b0a
Dephrase <davidjf123@gmail.com>
2021-06-27 15:19:03 +0100

    Addding handle submit button to buy form to take total price, stock bought, and stock amount

--- client/src/components/BuyStockForm.js
+++ client/src/components/BuyStockForm.js
@@ -17,19 +17,26 @@ const BuyStockForm = ({selectedStockInfo}) => {
     }
 
     const handleSharesAmountChange = (event) => {
-        console.log(event)
         setAmountOfShares(event.target.value)
+    }
+
+    const handleBuySubmit = (event) => {
+        event.preventDefault()
+        console.log(event)
+        const priceOfBuy = event.target[0]["form"][0]["value"]
+        
     }
 
     return (
         <>
-            <form>
+            <form onSubmit={handleBuySubmit}>
                 <label>{`Stock: ${selectedStockInfo["Meta Data"]["2. Symbol"]}`}</label>
                 <br></br>
                 <label>{`Price: $${getPrice()}`}</label>
+                <input type="hidden" name="amountPrice" value={getPrice()}></input>
                 <br></br>
                 <label>Number of shares: </label>
-                <input type="number" name="stockAmount" onChange={handleSharesAmountChange} required/>
+                <input type="number" name="amountStock" onChange={handleSharesAmountChange} required/>
                 <br></br>
                 <input type="submit" name="buy" value="Buy"/>
             </form>
