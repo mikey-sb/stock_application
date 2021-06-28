77d1f0b48b0c769e721f51508e04c4c5180e8eae
saadtarikk <82512601+saadtarikk@users.noreply.github.com>
2021-06-28 11:06:04 +0100

    wallet done

--- client/src/components/profile/ProfileWallet.js
+++ client/src/components/profile/ProfileWallet.js
@@ -1,37 +1,41 @@
 import React, {useState, useEffect} from 'react'
+import { updateStock } from '../../SharesServices';
 import {postCash, getCash, updateCash} from '../../WalletServices'
 
 const ProfileWallet = () => {
 
-    const [total, setTotal] = useState([]);
+    const [total, setTotal] = useState(0);
     const [inputNumber, setInputNumber] = useState(null)
     // const [stock, setStock] = useState("")
     // const [PurchasedPrice, setPurchasedPrice] = useState(0)
     // const [QuantityPurchased, setQuantityPurchased] = useState(0)
 
-    // useEffect(() => {
-    //     getCash()
-    //     .then((total) => {
-    //         setTotal(total)
-    //         console.log("asdasd")
+    useEffect(() => {
+        getCash()
+        .then((total) => {
+            setTotal(total[0])
+            console.log("asdasd")
 
-    //     })
-    // }, [])
+        })
+    }, [])
     
-    const addCash = (total) => {
-        setTotal(total)  //add cash should hold on the whole object not a number
+    // const addCash = (total) => {
+    //     setTotal(total)  //add cash should hold on the whole object not a number
 
-    }
+    // }
 
 
     const deposit = () => {
-        console.log(total)
-
-        setTotal(total + Number(inputNumber))
+        // console.log(total)
+        const copyTotal = {...total}
+        copyTotal.total_cash += Number(inputNumber)
+        setTotal(copyTotal)
     }
 
     const withdraw = () => {
-        setTotal(total - Number(inputNumber))
+        const copyTotal = {...total}
+        copyTotal.total_cash -= Number(inputNumber)
+        setTotal(copyTotal)
     }
     
     // const walletItem = total.map((cash) => {
@@ -41,24 +45,32 @@ const ProfileWallet = () => {
 
     const handleOnSubmit = (event) => {
         event.preventDefault()
-        console.log()
-        postCash(total).then((data) => {
-            addCash(data)
+        const copyTotal = {
+            total_cash: total.total_cash, 
+            profit: total.profit
+        }
+        updateCash(total._id, copyTotal).then((data) => {
+            // addCash(data)
+            console.log(data)
             }
         )
+    }
+
+    const handleNumberInput = (event) => {
+        setInputNumber(event.target.value)
     }
 
 
     return (
         <form onSubmit={handleOnSubmit}>
             <h1>WALLET</h1>
-            <input type="number" onChange={event => {setInputNumber(event.target.value)}} />
+            <input type="number" onChange={handleNumberInput} />
 
             <button onClick={() => deposit()}>
-          Submit
+          Deposit
             </button>
             <button onClick={() => withdraw()}>Withdraw</button>
-        <p>Total: </p>
+        <p>Total: {total.total_cash}</p>
         </form>
     )
 }
