f6ce60666413e9195f53119eeaa967c50d502599
saadtarikk <82512601+saadtarikk@users.noreply.github.com>
2021-06-29 16:11:15 +0100

    trying the current stock

--- client/src/components/profile/ProfilePerformance.js
+++ client/src/components/profile/ProfilePerformance.js
@@ -1,9 +1,16 @@
 import React from 'react'
 
-const ProfilePerformance = () => {
+const ProfilePerformance = ({ownedStocks}) => {
+
+    // console.log(ownedStocks)
 
 
-    
+    const uniqueStocks = ownedStocks.filter((stock) => {
+        return ownedStocks.indexOf(stock) === ownedStocks.lastIndexOf(stock)
+    })
+    console.log(uniqueStocks)
+
+
     return (
         <div>
             <h1>Profit/Loss Graph</h1>
